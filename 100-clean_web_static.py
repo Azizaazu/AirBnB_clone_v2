ba58c1f07a42becf160193eea33cd943d05705a9
Azizaazu <aynaou.aziza98@gmail.com>
2023-12-07 11:22:08 -0800

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -13,12 +13,13 @@ def do_clean(number=0):
     """
     Delete out-of-date archives in the versions and releases folders.
     """
-    try:
-        number = int(number)
-        if number < 1:
-            number = 1
-        with cd("/data/web_static/releases/"):
-            with cd(".."):
-                run("ls -1t | tail -n +{} | xargs rm -rf".format(number + 1))
-        with cd("/data/web_static/releases/"):
-            run("ls -1t | tail -n +{} | xargs rm -rf".format(number + 1))
+      number = int(number)
+
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
