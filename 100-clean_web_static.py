9a15c4d6c3aa7cdaeb64190cde9a3a5a964ad3e5
Azizaazu <aynaou.aziza98@gmail.com>
2023-12-07 11:52:38 -0800

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,6 +2,8 @@
 """ deletes out-of-date archives, using the function do_clean """
 
 from fabric.api import *
+from fabric.state import commands, connections
+import os.path
 
 env.hosts = ['54.237.68.111', '100.25.159.174']
 env.user = "ubuntu"
@@ -10,13 +12,24 @@ def do_clean(number=0):
     """
     Delete out-of-date archives in the versions and releases folders.
     """
-      number = int(number)
-
+    local('ls -t ~/AirBnB_Clone_V2/versions/').split()
+    with cd("/data/web_static/releases"):
+        target_R = sudo("ls -t .").split()
+    paths = "/data/web_static/releases"
+    number = int(number)
     if number == 0:
-        number = 2
+        num = 1
     else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+        num = number
+    if len(target_R) > 0:
+        if len(target) == number or len(target) == 0:
+            pass
+        else:
+            cl = target[num:]
+            for i in range(len(cl)):
+                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+        rem = target_R[num:]
+        for j in range(len(rem)):
+            sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
+    else:
+        pass
